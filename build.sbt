e8e622626dafebc9025a1f464e97d48cb1502358
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2022-10-17 02:35:45 +0200

    Leave the code reproducing the issue

--- build.sbt
+++ build.sbt
@@ -1,3 +1,3 @@
-scalaVersion := "3.2.0"
-//scalaVersion := "2.13.10"
+//scalaVersion := "3.2.0"
+scalaVersion := "2.13.10"
 libraryDependencies += "org.typelevel" %% "cats-effect" % "3.3.14"
--- main.scala
+++ main.scala
@@ -4,18 +4,18 @@ import cats.effect.unsafe.implicits.global
 
 object Configuration {
   println("000")
-  //Resource.eval(IO(println("AAA"))) // hangs in 2.13.10, works in 3.2.0
+  Resource.eval(IO(println("AAA"))) // hangs in 2.13.10, works in 3.2.0
   //Resource.eval(IO.blocking(println("AAA"))) // hangs in 2.13.10, works in 3.2.0
-  Resource.eval(IO.println("AAA")) // works in 2.13.10, works in 3.2.0
+  //Resource.eval(IO.println("AAA")) // works in 2.13.10, works in 3.2.0
     .allocated
     .unsafeRunSync()
   println("111")
 
   val unit = {
     println("000")
-    //Resource.eval(IO(println("AAA"))) // hangs in 2.13.10, works in 3.2.0
+    Resource.eval(IO(println("AAA"))) // hangs in 2.13.10, works in 3.2.0
     //Resource.eval(IO.blocking(println("AAA"))) // hangs in 2.13.10, works in 3.2.0
-    Resource.eval(IO.println("AAA")) // works in 2.13.10, works in 3.2.0
+    //Resource.eval(IO.println("AAA")) // works in 2.13.10, works in 3.2.0
       .allocated
       .unsafeRunSync()
     println("111")
